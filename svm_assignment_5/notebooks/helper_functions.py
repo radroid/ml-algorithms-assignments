5a3af9673a63528f0aa6d01b075273646fa5beab
Raj Dholakia <raj9dholakia@gmail.com>
2021-08-12 11:00:17 +0530

    Update helper_functions.py

--- svm_assignment_5/notebooks/helper_functions.py
+++ svm_assignment_5/notebooks/helper_functions.py
@@ -7,6 +7,78 @@ from sklearn.model_selection import cross_val_score, learning_curve, RepeatedKFo
 from sklearn.metrics import classification_report, confusion_matrix, auc
 
 
+def plot_conf_mat(*all_conf_mat, **format_settings):
+    """Plots the confusion matrix using Seaborn Heatmap.
+    
+    Args:
+        all_conf_mat (args as ndarray or pandas dataframe): The confusion matrix generated using sklearn's method.
+        format_settings (keywords arguements): 
+            titles_list (list of str): Titles to be given to each of the plots.
+            cmaps_list (list of str): cmap value to decide the color for each of the plots 
+                                     (refer to Matplotlib for possible values).
+            num_of_rows (int): number of rows of the plots.
+            num_of_cols (int): number of columns of the plots.
+        
+    Returns: None
+    """
+    num_of_plots = len(all_conf_mat)
+    num_of_rows, num_of_cols = get_ideal_plot_dim(num_of_plots)
+
+    all_titles = format_settings.get("titles_list", ["Confusion Matrix"]*num_of_plots)
+    all_cmaps = format_settings.get("cmaps_list", ['Blues']*num_of_plots)
+    nrows = format_settings.get("nrows", num_of_rows)
+    ncols = format_settings.get("ncols", num_of_cols)
+    figsize = format_settings.get("figsize", (12, 4))
+    
+    fig, axes = plt.subplots(nrows=nrows,
+                             ncols=ncols,
+                             figsize=figsize)
+    if not type(axes) == np.ndarray:
+        axes = [axes]
+    
+    for ax, conf_mat, title, cmap in zip(axes, all_conf_mat, all_titles, all_cmaps):
+        sns.heatmap(conf_mat, 
+                    annot=True,
+                    annot_kws={'size': 12},
+                    cbar=False,
+                    cmap=cmap,
+                    ax=ax)
+
+        ax.set_title(title, fontdict={'size': 16, 'weight': 'bold'}, pad=30)
+        ax.set_xlabel('Predicted Label', fontdict={'size': 14, 'weight': 'bold'}, labelpad=20)
+        ax.set_ylabel('True Label', fontdict={'size': 14, 'weight': 'bold'}, labelpad=20)
+        
+        loc_x = ax.get_xticks()
+        loc_y = ax.get_yticks()
+        loc_y -= 0.3
+
+        ax.set_xticklabels(['Edible', 'Poisonuous'], fontdict={"fontsize": 14})
+        ax.set_yticks(loc_y)
+        ax.set_yticklabels(['Edible', 'Poisonuous'], fontdict={"fontsize": 14}, rotation=90)
+        
+    plt.tight_layout(rect=[0, 0.03, 1.1, 0.97])
+
+
+def get_ideal_plot_dim(total_plots):
+    """Calculated the best number of rows and columns to create plots clearly.
+    
+    Args:
+        total_plots (int): Total number of plots to be created.
+    
+    Returns:
+        nrows (int): Number of rows to be plot.
+        ncols (int): Number of columns to be plot.
+    """
+    nrows = 1
+    ncols = 3
+    if total_plots <= 3:
+        ncols = total_plots
+        return nrows, ncols
+    else:
+        nrows = round(total_plots / 3) + 2
+        return nrows, ncols
+
+
 def metric_evaluation(model, X, y, cv: int = 5, print_results: bool = True):
     """
     The function evalutes the model on five metrics using cross validation scores for each and plots them.
@@ -43,8 +115,8 @@ def metric_evaluation(model, X, y, cv: int = 5, print_results: bool = True):
         performance_dict.update({metric: metric_score})
         
     performance_df = pd.DataFrame(performance_dict).round(3).T
-    performance_df.columns = [f"Cross Val {n}" for n in range(1, len(metric_score)+1)]
-    
+    performance_df.columns = [f"Fold {n}" for n in range(1, len(metric_score)+1)]
+
     # Instantiating figure
     fig, ax = plt.subplots(figsize=(10,7))
 
@@ -93,6 +165,9 @@ def plot_learning_curves(model, X, y):
         X (np.ndarray or pd.DataFrame): Contains the features from the dataset.
         y (np.ndarray or pd.DataFrame): Contains the target or response varible from the dataset.
     """
+    # Setting up random seed to ensure all models are evaluated on same data splits
+    np.random.seed(100)
+
     train_sizes, train_scores, test_scores = learning_curve(estimator=model,
                                                             X=X, 
                                                             y=y,
@@ -120,8 +195,8 @@ def plot_learning_curves(model, X, y):
     plt.show()
     
 
-def plot_box_plot(model, X, y, model_name: str = "Model", scoring: str = "recall_weighted",
-                  models: list = []):
+def plot_box_plot(model, X, y, cv: int = 5, model_name: str = "Model", scoring: str = "recall_weighted",
+                  models: list = None):
     """The function plots a box plot for the scoring parameter defined.
 
     Args:
@@ -132,8 +207,11 @@ def plot_box_plot(model, X, y, model_name: str = "Model", scoring: str = "recall
         scoring (str, optional): The scoring parameter to be used. Defaults to "recall_weighted".
         models (list, optional): A list of `model`s. Defaults to [].
     """
+    # Setting up random seed to ensure all models are evaluated on same data splits
+    np.random.seed(100)
 
-    if not models:
+    if models is None:
+        models = []
         models.append((model_name, model))
 
     results =[]
@@ -142,8 +220,8 @@ def plot_box_plot(model, X, y, model_name: str = "Model", scoring: str = "recall
     metric = scoring.replace('_', ' ').capitalize()
     print(f'Model Evaluation - {metric}')
     for name, model in models:
-        rkf = RepeatedKFold(n_splits=10, n_repeats=5, random_state=100)
-        cv_results = cross_val_score(model, X, y, cv=rkf, scoring=scoring)
+        # rkf = RepeatedKFold(n_splits=10, n_repeats=5, random_state=100)
+        cv_results = cross_val_score(model, X, y, cv=cv, scoring=scoring)
         results.append(cv_results)
         names.append(name)
         print('{} {:.2f} +/- {:.2f}'.format(name,cv_results.mean(),cv_results.std()))
@@ -160,14 +238,16 @@ def plot_box_plot(model, X, y, model_name: str = "Model", scoring: str = "recall
     pass
 
 
-def full_model_evaluation(model, X, y, model_name: str = None):
+def full_model_evaluation(model, X, y, cv: int = 5, model_name: str = None):
     """
     The function does the following 4 things:
     1. Plot the learning curve.
     2. Plot a boxplot for weighted recall.
     3. Plot the cross validation scores of 5 metrics.
     """
-    
+    # Setting up random seed to ensure all models are evaluated on same data splits
+    np.random.seed(100)
+
     if model_name is None:
         model_name = "Decision Tree"
 
@@ -176,7 +256,7 @@ def full_model_evaluation(model, X, y, model_name: str = None):
     plot_learning_curves(model, X, y)
     
     # Model Evaluation - Boxplot
-    plot_box_plot(model, X, y, model_name)
+    plot_box_plot(model, X, y, cv=cv, model_name=model_name)
     
     # Evaluate the performance
     metric_evaluation(model, X, y)
